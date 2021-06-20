72777cff307c53297794ff928c9afde630b0a28b
Ariel Sardinha <odararepresentacoes@outlook.com.br>
2021-06-20 13:44:48 -0300

    colocando os computed properties para poder buscar pelo nome

--- js/listComponents.js
+++ js/listComponents.js
@@ -1,12 +1,17 @@
 Vue.component("my-list", {
   template: `
         <li class="poke-list-item">
-          <img
-            v-bind:src="'//serebii.net/pokedex-xy/icon/'+pokemon.number+'.png'"
-            alt=""
-          />
+          <img v-bind:src="sprite"/>
           <span>{{pokemon.name}} - {{pokemon.number | pokeNumber}}</span>
         </li>
   `,
   props: ["pokemon"],
+  computed: {
+    number: function () {
+      return Vue.filter("pokeNumber")(this.pokemon.number);
+    },
+    sprite: function () {
+      return `//serebii.net/pokedex-xy/icon/${this.number}.png`;
+    },
+  },
 });
